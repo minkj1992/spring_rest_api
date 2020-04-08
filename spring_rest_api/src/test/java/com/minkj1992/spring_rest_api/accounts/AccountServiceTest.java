a5c01faf0041bfd2478b4549eceede806a01fbec
minkj1992 <minkj1992@gmail.com>
2020-04-08 21:48:33 +0900

    findByUsernameFail() test2 update

--- spring_rest_api/src/test/java/com/minkj1992/spring_rest_api/accounts/AccountServiceTest.java
+++ spring_rest_api/src/test/java/com/minkj1992/spring_rest_api/accounts/AccountServiceTest.java
@@ -1,11 +1,14 @@
 package com.minkj1992.spring_rest_api.accounts;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,11 +16,15 @@ import javax.swing.*;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException;
 
     @Autowired
     AccountService accountService;
@@ -26,7 +33,7 @@ public class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
-    public void findByUserName() throws Exception {
+    public void findByUsername() throws Exception {
         //given
         String password = "minkj1992";
         String userName = "minkj1992@gmail.com";
@@ -41,6 +48,20 @@ public class AccountServiceTest {
         UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
         //then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void findByUsernameFail() throws Exception {
+        //given
+        String username = "random@email.com";
+        //when & then
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e) {
+            // username에 대한 메시지까지 확인
+            assertThat(e.getMessage()).containsSequence(username);
+        }
     }
 
 }
